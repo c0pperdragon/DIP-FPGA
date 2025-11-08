feafdf0d57b7e86776e7e0dab8239a06298b6b49
c0pperdragon <c0pperdragon50@gmail.com>
2025-11-08 14:18:05 +0100

    Investigate possibility to make a video PLL with 1ns jitter.

--- XO2-DIP20/examples/VideoPLL/source/VideoPLL.vhd
+++ XO2-DIP20/examples/VideoPLL/source/VideoPLL.vhd
@@ -5,8 +5,12 @@ use ieee.std_logic_1164.all;
 entity VideoPLL is	
 	port (
 		CLK10        : in std_logic;   -- reference clock
-		VIDEO        : in std_logic;   -- video signal (using differencial pair to detect sync)
-		PIXELCLK     : out std_logic
+		CSYNC        : out std_logic;  -- synthethic sync train
+		
+		VIDEO        : in std_logic;   -- csync input of input video signal 
+		PIXELCLK     : out std_logic;
+		
+		TESTCLK      : out std_logic_vector(3 downto 0)
 	);	
 	
 	ATTRIBUTE IO_TYPES : string;
@@ -16,21 +20,61 @@ end entity;
  
 architecture immediate of VideoPLL is
 
-	component PLL is
+	component PLL_8x120 is
     port (
         CLKI: in  std_logic; 
-        CLKOP: out  std_logic);
+        CLKOP: out  std_logic; 
+        CLKOS: out  std_logic; 
+        CLKOS2: out  std_logic; 
+        CLKOS3: out  std_logic);
 	end component;
 	
-	signal CLK120:std_logic;
+	signal CLK0,CLK1,CLK2,CLK3:std_logic;
 begin
 	
-	pll0: PLL port map ( CLK10, CLK120 );
-
-	process (CLK120) 
+	pll0: PLL_8x120 port map ( CLK10, CLK0, CLK1, CLK2, CLK3 );
+	
+	-- create synthetic csync pulses
+	process (CLK10)
+	variable x: integer range 0 to 1023 := 0;
+	variable y: integer range 0 to 511 := 0;
 	begin
-		if rising_edge(CLK120) then
+		if rising_edge(CLK10) then
+			CSYNC <= '1';
+			if y<3 then
+				if x<47 then
+					CSYNC <= '0';
+				end if;
+			else
+				if x<640-47 then
+					CSYNC <= '0';
+				end if;
+			end if;
+				
+			if x<640-1 then
+				x := x+1;
+			else
+				x := 0;
+				if y<312-1 then
+					y := y+1;
+				else
+					y := 0;
+				end if;
+			end if;		
+		end if;	
+	end process;
+	
+	-- subdivide to pixels
+	process (CLK0) 
+	begin
+		if rising_edge(CLK0) then
 			PIXELCLK <= VIDEO;
 		end if;
 	end process;
+	
+	process (CLK0,CLK1,CLK2,CLK3)
+	begin
+		TESTCLK <= CLK3 & CLK2 & CLK1 & CLK0;
+	end process;
+	
 end immediate;
